9d18d4b4a9117c3671deea0b5d5f71c9176eeb1d
Amadejus Valionis <amadeus444@gmail.com>
2021-05-11 19:20:08 +0300

    add global background color and padding to main

--- eshop/src/App.jsx
+++ eshop/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <header>
         <Navbar />
       </header>
-      <main>
+      <main className="p-2">
         <Switch>
           <Route path="/vision">
             <Vision />
