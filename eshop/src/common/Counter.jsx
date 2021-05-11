cb8331b65fe1b5982eed98da9f5f20b07d61cc7d
Amadejus Valionis <amadeus444@gmail.com>
2021-05-11 20:03:15 +0300

    add increment/decrement functionality

--- eshop/src/common/Counter.jsx
+++ eshop/src/common/Counter.jsx
@@ -3,11 +3,19 @@ import React, { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
 
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
   return (
     <div>
-      <button>-</button>
+      <button onClick={decrement}>-</button>
       <input value={count}></input>
-      <button>+</button>
+      <button onClick={increment}>+</button>
     </div>
   );
 }
