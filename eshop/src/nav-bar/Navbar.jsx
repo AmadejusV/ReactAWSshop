f99659d69e104d4a84f5d980e69a9e390bf821ee
Amadejus Valionis <amadeus444@gmail.com>
2021-05-11 19:11:06 +0300

    add font sans to body

--- eshop/src/nav-bar/Navbar.jsx
+++ eshop/src/nav-bar/Navbar.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
-  return <nav>navbar</nav>;
+  return (
+    <nav className="bg-blue-300">
+      <ul className="flex">
+        <li className="p-2">
+          <Link to="/">Products</Link>
+        </li>
+        <li className="p-2">
+          <Link to="/vision">Vision</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
