678034d1ab51fc94ace61d2af4dadbd021ca406f
Amadėjus Valionis <amadeus444@gmail.com>
2021-06-08 18:34:07 +0300

    btn component styling

--- eshop/src/common/components/Counter.jsx
+++ eshop/src/common/components/Counter.jsx
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import Button from "./Button";
+
+const buttonClass = "bg-white w-6 p-2 text-blue-200 focus:outline-none ";
+const btnGerulisStyle =
+  "font-bold transform scale-125 duration-200 hover:translate-y-0.5 active:translate-y-0.5";
 
 function Counter() {
   const [count, setCount] = useState(() => {
@@ -29,24 +34,18 @@ function Counter() {
 
   return (
     <div className="border-blue-300 border-2 inline-block mr-2">
-      <button
-        className="bg-white w-6 text-blue-200 focus:outline-none"
-        onClick={decrement}
-      >
+      <Button className={buttonClass} onClick={decrement}>
         -
-      </button>
+      </Button>
       <input
-        className="appearance-none w-8 text-center focus:outline-none"
+        className="appearance-none w-10 text-center focus:outline-none p-2"
         type="number"
         onChange={userInputHandler}
         value={count}
       ></input>
-      <button
-        className="bg-white w-6 text-blue-200 focus:outline-none"
-        onClick={increment}
-      >
+      <Button className={buttonClass} onClick={increment}>
         +
-      </button>
+      </Button>
     </div>
   );
 }
