b0ee4aa0c048fa87e329428be5ba3787c8798c0b
Amadėjus Valionis <amadeus444@gmail.com>
2021-06-15 18:33:26 +0300

    spinner dots less pushy | interval time set as constant | dependency removed from useEffect

--- eshop/src/common/components/Spinner.jsx
+++ eshop/src/common/components/Spinner.jsx
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from "react";
 import { ReactComponent as SpinIcon } from "../assets/spin.svg";
 
+const dotsIntervalInMs = 700;
+
 function Spinner({ text = "Loading..." }) {
   const [dots, setDots] = useState("");
 
   useEffect(() => {
     const incrementingTimer = setInterval(() => {
       setDots((prevState) => (prevState.length >= 3 ? "" : prevState + "."));
-    }, 700);
+    }, dotsIntervalInMs);
 
     console.log(dots);
     return () => clearInterval(incrementingTimer);
-  }, [dots]);
+  }, []);
 
   return (
     <div className="h-full flex flex-col justify-center items-center">
       <SpinIcon />
-      <div className="mt-1 font-semibold">
-        {text}
-        {dots}
+      <div className="mt-1 font-semibold flex">
+        <span>{text}</span>
+        <span className="w-3">{dots}</span>
       </div>
     </div>
   );
