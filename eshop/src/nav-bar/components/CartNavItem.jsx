a153bc4e947c48d30271a417657283d483425cfc
Amadėjus Valionis <amadeus444@gmail.com>
2021-07-12 18:45:39 +0300

    Add cart cartProvider cartNavItem

---
+++ eshop/src/nav-bar/components/CartNavItem.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import NavItem from "./NavItem";
+import { ReactComponent as CartIcon } from "../../common/assets/cart.svg";
+import { CartContext } from "../../common/providers/CartProvider";
+
+function CartIconWithNo() {
+  const { cart } = useContext(CartContext);
+  return (
+    <>
+      <CartIcon className="w-8 h-8"></CartIcon>
+      <span
+        className="absolute text-yellow-400 text-xs font-bold"
+        style={{ right: "35%", top: "25%" }}
+      >
+        {cart?.products?.length}
+      </span>
+    </>
+  );
+}
+
+function CartNavItem() {
+  return (
+    <NavItem
+      route="/cart"
+      content={<CartIconWithNo />}
+      className="ml-auto mr-2 relative"
+    />
+  );
+}
+
+export default CartNavItem;
